e5343f494ba20e61938c1f618aaee5f95c083f52
RamBabu1993 <rambabu1993ism@gmail.com>
2026-05-15 19:02:28 +0000

    Deploying to gh-pages from @ RamBabu1993/RamBabu1993.github.io@bfc44d4de22263abc0545e528c29a04b4c2be106 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "RamBabu1993/rambabu.github.io",
+    "data-repo": "RamBabu1993/RamBabu1993.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
