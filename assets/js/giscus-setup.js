07b83586c3a95cd2e0969ab9913a2ab22754e1db
rambaburanga <rambabu1993ism@gmail.com>
2026-05-16 06:27:21 +0000

    Deploying to gh-pages from @ rambaburanga/rambaburanga.github.io@d212b34b4ba941d02988e0bc2e66852f5ad4877e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "RamBabu1993/RamBabu1993.github.io",
+    "data-repo": "rambaburanga/rambaburanga.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
